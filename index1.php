8e1a5deb737799dd9b4fed56ca55d14c5b9171a5
Ondřej Šplíchal <62213564+OndanCZe@users.noreply.github.com>
2020-04-17 10:33:50 +0200

    Create index1.php

---
+++ index1.php
@@ -0,0 +1,14 @@
+<?php
+$a=7;
+$b=7;
+var_dump($a+$b);
+var_dump($a*$b);
+var_dump($a/$b);
+var_dump($a-$b);
+
+var_dump($a!=$b);
+var_dump($a=$b);
+var_dump($b);
+var_dump($a);
+var_dump($a==$b);
+var_dump($a===$b);
